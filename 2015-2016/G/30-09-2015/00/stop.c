50d2b0d14aa6312b7b98bc13f61689f904e11c47
Tsvetelina Borisova <ts.borisova3@gmail.com>
2015-09-30 19:12:54 +0300

    Added STOP program

--- 2015-2016/G/30-09-2015/00/stop.c
+++ 2015-2016/G/30-09-2015/00/stop.c
@@ -8,7 +8,23 @@ char word[100];
 do {
 
 scanf("%s", word);
-printf("%s\n", word);
+
+
+// Check result when add "Hello World Stop" should stop?
+
+if (word[0] == 'S' && word[1] == 'T' && word[2] == 'O' && word[3] == 'P') {
+    break;
+} else {
+    
+    int e = 0;
+    for (; e < 100; e++) {
+        if (word[e] >= 'a' && word[e] <= 'z') {
+            word[e] -= 32;
+        }
+    }
+
+    printf("%s\n", word);
+}
 
 } while(1);
 
