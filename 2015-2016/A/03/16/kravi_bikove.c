95daa6b66cd97963664ef4bc2076124b2bce718b
iordanivanov <iiordan.ivanov@gmail.com>
2015-10-20 15:55:11 +0000

    Create kravi_bikove.c
    Обща работа между 05 и 16 номер от 10А клас. Програмата не е в завършен вид.

---
+++ 2015-2016/A/03/16/kravi_bikove.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void main(void)
+{
+        printf("Vuvedete niz ot 4 cifri molq.\n");
+        int niz[2][4];
+        scanf("\n%d", niz[1][1]);
+        scanf("\n%d", niz[1][2]);
+        scanf("\n%d", niz[1][3]);
+        scanf("\n%d", niz[1][4]);
+
+         printf("\nVuvedete vtori niz ot 4 cifri molq.\n");
+        scanf("\n%d", niz[2][1]); 
+        scanf("\n%d", niz[2][2]);
+        scanf("\n%d", niz[2][3]);
+        scanf("\n%d", niz[2][4]);
+
+        int c1, c2;
+        c1 = 1; 
+        c2 = 1;
+
+        while(1==1)
+        {
+                c1 = 1;
+        }
+}
