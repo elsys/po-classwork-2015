41679a32d878eeea5e736c0ed67722909c1b420c
Yellobelly <alexstoyanof@gmail.com>
2015-10-20 19:01:16 +0300

    cowsandbulls

---
+++ 2015-2016/A/01/03/file.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main ()
+{
+int s,in;
+int bulls=0,cows=0,i;
+printf ("vuvedi chislo za igra");
+fgets (s,5,stdin);
+while (1>0)
+{
+printf("vuvedi predpolagaemo chislo");
+scanf("%d");
+
+//bull check
+while (i=0;i<4;i++)
+{
+
+}
+
+}
+}
